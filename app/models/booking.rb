fdda8ccdd373fe81a4afb687f839d7dccec3dd3e
Basile Rieu <basilerieu@yahoo.fr>
2020-11-17 11:42:23 +0100

    model otter et booking ok

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :otter
+
+  validates :beginning_date, uniqueness: true, presence: true
+  validates :ending_date, uniqueness: true, presence: true
 end
--- app/models/otter.rb
+++ app/models/otter.rb
@@ -1,5 +1,6 @@
 class Otter < ApplicationRecord
   belongs_to :user
+  has_many :reviews, through: :bookings
 
   validates :name, presence: true
 end
