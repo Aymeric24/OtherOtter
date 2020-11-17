2cab4ce287b071b85855e0842e8fe10ac666f69f
Basile Rieu <basilerieu@yahoo.fr>
2020-11-17 15:11:05 +0100

    Pundit added and navbar with links

--- app/controllers/otters_controller.rb
+++ app/controllers/otters_controller.rb
@@ -1,16 +1,41 @@
 class OttersController < ApplicationController
   def index
-    @otters = Otter.all
+    @otters = policy_scope(Otter).order(created_at: :desc)
+  end
+
+  def show
+    @otter = Otter.find(params[:id])
   end
 
   def new
     @otter = Otter.new
+    authorize @otter
   end
 
   def create
     @otter = Otter.new(otter_params)
-    @otter.save
+    authorize @otter
+    if @otter.save
+      redirect_to otter_path(@otter)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @otter = Otter.find(params[:id])
+  end
+
+  def update
+    @otter = Otter.find(params[:id])
+    @otter.update(otter_params)
     redirect_to otter_path(@otter)
+  end
+
+  def destroy
+    @otter = Otter.find(params[:id])
+    @otter.destroy
+    redirect_to otters_path
   end
 
   private
