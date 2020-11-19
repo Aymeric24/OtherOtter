b97e6bd52e3efb1f68fdfe172a00e5d85e8daad5
Aymeric Faidherbe <aymeric.faidherbe@gmail.com>
2020-11-19 20:58:39 +0100

    Removing precompiling stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,6 +115,4 @@ Rails.application.configure do
 
   # Must include to get inline SVGs to work in deploy
   config.assets.css_compressor = :sass
-
-  config.assets.initialize_on_precompile = false
 end
